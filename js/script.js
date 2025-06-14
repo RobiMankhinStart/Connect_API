62edf512ccef7a3d0ad44029c4c26c4697ba5a91
RobiMankhinStart <mankhinrobi@gmail.com>
2025-06-14 06:19:49 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,118 @@
+// ................Dom creation
+let mainDiv = document.querySelector(".mainDiv");
+let inputSearchDiv = document.querySelector(".inputSearchDiv");
+let inputSearch = document.querySelector(".inputSearch");
+let searchButton = document.querySelector(".searchButton");
+let errorMessage = document.querySelector(".errorMessage");
+
+let userListDiv = document.createElement("div");
+userListDiv.classList.add("userListDiv");
+mainDiv.appendChild(userListDiv);
+
+let allData = [];
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    allData = data;
+
+    data.map((item) => {
+      //....... dom creation
+      let singleUser = document.createElement("div");
+      let imgDiv = document.createElement("div");
+      let name = document.createElement("h2");
+      let username = document.createElement("h3");
+      let email = document.createElement("h3");
+
+      // adding clasName
+      singleUser.classList.add("singleUser");
+      imgDiv.classList.add("imgDiv");
+      name.classList.add("name");
+      username.classList.add("username");
+      email.classList.add("email");
+
+      // attaching value in dom
+
+      name.innerHTML = `Name : ${item.name}`;
+      username.innerHTML = `User Name : ${item.username}`;
+      email.innerHTML = `Email : ${item.email}`;
+
+      // appending
+      singleUser.appendChild(imgDiv);
+      singleUser.appendChild(name);
+      singleUser.appendChild(username);
+      singleUser.appendChild(email);
+      userListDiv.appendChild(singleUser);
+    });
+  });
+// search button click function
+searchButton.addEventListener("click", () => {
+  if (!inputSearch.value) {
+    errorMessage.innerHTML = "Enter any name";
+    allData.map((item) => {
+      //....... dom creation
+      let singleUser = document.createElement("div");
+      let imgDiv = document.createElement("div");
+      let name = document.createElement("h2");
+      let username = document.createElement("h3");
+      let email = document.createElement("h3");
+
+      // adding clasName
+      singleUser.classList.add("singleUser");
+      imgDiv.classList.add("imgDiv");
+      name.classList.add("name");
+      username.classList.add("username");
+      email.classList.add("email");
+
+      // attaching value in dom
+
+      name.innerHTML = `Name : ${item.name}`;
+      username.innerHTML = `User Name : ${item.username}`;
+      email.innerHTML = `Email : ${item.email}`;
+
+      // appending
+      singleUser.appendChild(imgDiv);
+      singleUser.appendChild(name);
+      singleUser.appendChild(username);
+      singleUser.appendChild(email);
+      userListDiv.appendChild(singleUser);
+    });
+  } else {
+    const searchdItem = allData.find((i) => i.username == inputSearch.value);
+
+    if (!searchdItem) {
+      errorMessage.innerHTML = "no item found";
+      userListDiv.innerHTML = "";
+    } else {
+      errorMessage.innerHTML = "";
+      userListDiv.innerHTML = "";
+
+      console.log(allData);
+
+      //....... dom creation
+      let singleUser = document.createElement("div");
+      let imgDiv = document.createElement("div");
+      let name = document.createElement("h2");
+      let username = document.createElement("h3");
+      let email = document.createElement("h3");
+
+      // adding clasName
+      singleUser.classList.add("singleUser");
+      imgDiv.classList.add("imgDiv");
+      name.classList.add("name");
+      username.classList.add("username");
+      email.classList.add("email");
+
+      // appending
+      singleUser.appendChild(imgDiv);
+      singleUser.appendChild(name);
+      singleUser.appendChild(username);
+      singleUser.appendChild(email);
+      userListDiv.appendChild(singleUser);
+
+      // attaching value in dom
+      name.innerHTML = `Name : ${searchdItem.name}`;
+      username.innerHTML = `User Name : ${searchdItem.username}`;
+      email.innerHTML = `Email : ${searchdItem.email}`;
+    }
+  }
+});
